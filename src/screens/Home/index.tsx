dded39015be41081e009eacf0d1cf6a7b32706da
Ernane <ernanecarvalho89@gmail.com>
2025-02-21 16:21:14 -0300

    removeItens

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,29 @@
-import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
 
-import { Participant } from "../components/Participant";
+import { Participant } from "../../components/Participant";
 
-import { styles } from "./style";
+import { styles } from "./styles";
 
 export function Home() {
   const participants = ['Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'João'];
   
   function handleParticipantAdd() {
-    console.log("Você clicou no botão de Adicionar!");
+    if (participants.includes("Rodrigo")) {
+      return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
+    }
   }
 
   function handleParticipantRemove(name: string) {
-    console.log(`Você clicou em remover o participante ${name}`);
+    Alert.alert("Remover", `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert("Deletado!")
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
   }
 
   return (
@@ -46,7 +57,7 @@ export function Home() {
           <Participant 
             key={item} 
             name={item} 
-            onRemove={() => handleParticipantRemove("Rodrigo")} 
+            onRemove={() => handleParticipantRemove(item)} 
           />
         )}
         showsVerticalScrollIndicator={false}
